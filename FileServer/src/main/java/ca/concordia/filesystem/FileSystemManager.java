ce595fdc0eec87b68aae08f9162987fe8b188ca2
Gabriel Lauture <gabriel@eos-gnss.com>
2025-11-11 20:28:30 -0500

    Beginning of initialization

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -2,6 +2,7 @@ package ca.concordia.filesystem;
 
 import ca.concordia.filesystem.datastructures.FEntry;
 
+import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -9,19 +10,24 @@ public class FileSystemManager {
 
     private final int MAXFILES = 5;
     private final int MAXBLOCKS = 10;
-    private final static FileSystemManager instance;
+    private static FileSystemManager instance;
     private final RandomAccessFile disk;
-    private final ReentrantLock globalLock = new ReentrantLock();
+    private ReentrantLock globalLock = new ReentrantLock();
 
     private static final int BLOCK_SIZE = 128; // Example block size
 
     private FEntry[] inodeTable; // Array of inodes
     private boolean[] freeBlockList; // Bitmap for free blocks
 
-    public FileSystemManager(String filename, int totalSize) {
+    public FileSystemManager(String filename, int totalSize) throws FileNotFoundException {
         // Initialize the file system manager with a file
-        if(instance == null) {
-            //TODO Initialize the file system
+        if (instance == null) {
+            disk = new RandomAccessFile(filename, "rw");
+            inodeTable = new FEntry[MAXFILES];
+            for (int i = 0; i < MAXFILES; i++) {
+                inodeTable[i] = new FEntry("", (short) 0, (short) -1);
+            }
+            // TODO Initialize the file system
         } else {
             throw new IllegalStateException("FileSystemManager is already initialized.");
         }
@@ -33,6 +39,26 @@ public class FileSystemManager {
         throw new UnsupportedOperationException("Method not implemented yet.");
     }
 
+    public void deleteFile(String filenName) throws Exception {
+        // Todo
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
 
-    // TODO: Add readFile, writeFile and other required methods,
+    public void writeFile(String fileName, byte[] contents) throws Exception {
+        // Todo
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
+
+    public byte[] readFile(String fileName) throws Exception {
+        // Todo
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
+
+    public String[] listFiles() {
+        // Todo
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
+
 }
+
+// TODO: Add readFile, writeFile and other required methods,
